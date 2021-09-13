06d27e73823f810109cc5b052563c4e9f322277a
Juan Camilo Garcia <camilo456garcia@gmail.com>
2021-09-12 22:28:36 -0500

    header desktop

--- src/index.js
+++ src/index.js
@@ -2,21 +2,17 @@ import "./styles/main.scss"
 import Swiper, { Navigation, Pagination } from 'swiper';
 
 const swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1.2,
-  spaceBetween: 10,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    loop: true,
-    breakpoints: {
-      640: {
-        slidesPerView: 2,
-        spaceBetween: 20,
-      },
-      768: {
-        slidesPerView: 3,
-        spaceBetween: 40,
-      },
+  slidesPerView: 1,
+  spaceBetween: 20,
+  clickable: true,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
     },
-  },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  }
 });
